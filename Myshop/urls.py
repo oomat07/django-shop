b8174ebcc9ce538b063be93411153b1e5370ead5
oomat07 <shakiralievomat3@gmail.com>
2024-03-22 17:48:56 +0600

    added users folder

--- Myshop/urls.py
+++ Myshop/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.shop.urls')),
+    path('accounts', include('apps.users.urls'))
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
