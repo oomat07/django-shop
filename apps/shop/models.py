8d50a5de365b74cafe754d7121aa69ecd1bdcb8c
oomat07 <shakiralievomat3@gmail.com>
2024-03-22 15:59:33 +0600

    created models for ordering

--- apps/shop/models.py
+++ apps/shop/models.py
@@ -23,3 +23,31 @@ class Product(models.Model):
 
     def __str__(self) -> str:
         return self.name
+    
+
+class Order(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    address = models.CharField(max_length=225)
+    postal_code = models.CharField(max_length=225)
+    city = models.CharField(max_length=225)
+    created = models.DateField(auto_now_add=True)
+    update = models.DateField(auto_now_add=True)
+    paid = models.BooleanField(default=True)
+
+    class Meta:
+         ordering = ('-created',)
+
+    def __str__(self):
+         return f"Order {self.id}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name = 'items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name = 'order_items', on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
